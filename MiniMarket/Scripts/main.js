1e0543ae15a4d9cc4451c4a464273d7ab0ff12ef
Thomas <thomascaycedo19@outlook.com>
2018-05-08 00:15:44 -0500

    Se incluye slideshow

--- MiniMarket/Scripts/main.js
+++ MiniMarket/Scripts/main.js
@@ -32,3 +32,21 @@ clientes.controller("clientescontroller", function ($scope, listaclientes) {
 
     });
 });
+
+/*=============================================
+OBJETO CON LAS PROPIEDADES DEL SLIDE
+=============================================*/
+
+var p = {
+
+    paginacion: document.querySelectorAll("#paginacion li"),
+    item: 0,
+    cajaSlide: document.querySelector("#slide ul"),
+    animacionSlide: "slide",
+    imgSlide: document.querySelectorAll("#slide ul li"),
+    avanzar: document.querySelector("#slide #avanzar"),
+    retroceder: document.querySelector("#slide #retroceder"),
+    velocidadSlide: 3000,
+    formatearLoop: false
+}
+
